86f7d08ec8dd715fee6e3227b33b0e5759996305
Daan van Rasmhorst <daan@vanramshor.st>
2023-09-14 17:06:18 +0200

    updated query

--- wikidata.js
+++ wikidata.js
@@ -1,12 +1,14 @@
 query = `
-# List of spas with coordinates, images, Wikicommons categories, and official websites in English
-SELECT ?spa ?spaLabel ?coordinate (SAMPLE(?image) AS ?selectedImage) ?commonsCategory (SAMPLE(?officialWebsite) AS ?website) WHERE {
+# List of spas with one coordinate, images, Wikicommons categories, and official websites in English
+SELECT ?spa ?spaLabel (SAMPLE(?coordinate) AS ?selectedCoordinate) (SAMPLE(?image) AS ?selectedImage) (SAMPLE(?commonsCategory) AS ?selectedCategory) (SAMPLE(?officialWebsite) AS ?website) WHERE {
   VALUES ?spaTypes { 
     wd:Q1341387 
     # wd:Q785952 
     wd:Q1004887 
     wd:Q28077 
     wd:Q57036 
+    wd:Q177380 
+    wd:Q34338776 
   }
   ?spa wdt:P31 ?spaTypes.
   FILTER NOT EXISTS { ?spa wdt:P31 wd:Q7930989. } # Exclude instances of city or town
@@ -19,5 +21,5 @@ SELECT ?spa ?spaLabel ?coordinate (SAMPLE(?image) AS ?selectedImage) ?commonsCat
   }
   SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
 }
-GROUP BY ?spa ?spaLabel ?coordinate ?commonsCategory
+GROUP BY ?spa ?spaLabel
 `
