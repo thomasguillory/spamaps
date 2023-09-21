864391ceb692c9accf2f9b2e1316df0f74763c2e
Daan van Rasmhorst <daan@vanramshor.st>
2023-09-21 09:14:59 +0200

    Update overpas_turbo.js

--- overpas_turbo.js
+++ overpas_turbo.js
@@ -1,10 +1,14 @@
+
+
+
 // query info for overpass turbo. 
 "https://overpass-turbo.eu/"
 
 // al categories below could be any of those geometries types:
 node, way, relation // for simplicity, we will use only node type in this example.
 
-// identifiede categories:
+
+// identifiede categories with the aproximat numbers:
 node["amenity"="spa"];          // 688      // https://taginfo.openstreetmap.org/tags/amenity=spa#overview
 node["amenity"="SPA"];          // 2
 node["amenity"="spa_resort"];   // 4
@@ -34,56 +38,51 @@ node["spa"]["spa"!="no"]; // 275   // query part for: “spa=*” exept spa=no  
 
 
 
-// buildign relevant query:
+// Complete query code for overpas turbo website:
+    // Loaded – nodes: 90046, ways: 8976, relations: 122
+    // Displayed – pois: 14978, lines: 29, polygons: 8694
 
-// problem, the website UI gives only 24000 results but the query should gives 100 000 results... maybe trying the API?
-    //Loaded –      nodes: 93217,   ways: 9122,     relations: 133
-    //Displayed –   pois: 15113,    lines: 33,      polygons: 8810
 [out:json][timeout:300];
 (
-node["amenity"="spa"];
-way["amenity"="spa"];
-relation["amenity"="spa"];
+    node["amenity"="spa"];
+    way["amenity"="spa"];
+    relation["amenity"="spa"];
 
-node["bath:type"="thermal"];
-way["bath:type"="thermal"];
-relation["bath:type"="thermal"];
+    node["bath:type"="thermal"];
+    way["bath:type"="thermal"];
+    relation["bath:type"="thermal"];
 
-node["bath:type"="onsen"];
-way["bath:type"="onsen"];
-relation["bath:type"="onsen"];
+    node["bath:type"="onsen"];
+    way["bath:type"="onsen"];
+    relation["bath:type"="onsen"];
 
-node["bath:type"="hot_spring"];
-way["bath:type"="hot_spring"];
-relation["bath:type"="hot_spring"];
+    node["bath:type"="hot_spring"];
+    way["bath:type"="hot_spring"];
+    relation["bath:type"="hot_spring"];
 
-node["bath:type"="lake"];
-way["bath:type"="lake"];
-relation["bath:type"="lake"];
+    node["natural"="hot_spring"];
+    way["natural"="hot_spring"];
+    relation["natural"="hot_spring"];
 
-node["natural"="hot_spring"];
-way["natural"="hot_spring"];
-relation["natural"="hot_spring"];
+    node["leisure"="spa"];
+    way["leisure"="spa"];
+    relation["leisure"="spa"];
 
-node["leisure"="spa"];
-way["leisure"="spa"];
-relation["leisure"="spa"];
+    node["leisure"="sauna"];
+    way["leisure"="sauna"];
+    relation["leisure"="sauna"];
 
-node["leisure"="sauna"];
-way["leisure"="sauna"];
-relation["leisure"="sauna"];
+    node["leisure"="hot_spring"];
+    way["leisure"="hot_spring"];
+    relation["leisure"="hot_spring"];
 
-node["leisure"="hot_spring"];
-way["leisure"="hot_spring"];
-relation["leisure"="hot_spring"];
+    node["tourism"="spa_resort"];
+    way["tourism"="spa_resort"];
+    relation["tourism"="spa_resort"];
 
-node["tourism"="spa_resort"];
-way["tourism"="spa_resort"];
-relation["tourism"="spa_resort"];
-
-node["spa"]["spa"!="no"];
-way["spa"]["spa"!="no"];
-relation["spa"]["spa"!="no"];
+    node["spa"]["spa"!="no"];
+    way["spa"]["spa"!="no"];
+    relation["spa"]["spa"!="no"];
 );
 out body;
 >;
@@ -91,10 +90,7 @@ out skel qt;
 
 
 
-
-
-
-// exemple query code:
+// exemple query code for overpas turbo website:
     [out:json][timeout:300];
     (
         node["amenity"="public_bath"]["bath:type"="thermal"];
@@ -104,3 +100,56 @@ out skel qt;
     out body;
     >;
     out skel qt;
+
+
+
+
+
+// the following terminal command will download the result of the query in a json file:
+
+curl -X POST -d '
+[out:json];
+(
+    node["leisure"="sauna"];
+    way["leisure"="sauna"];
+    relation["leisure"="sauna"];
+
+    node["spa"]["spa"!="no"];
+    way["spa"]["spa"!="no"];
+    relation["spa"]["spa"!="no"];
+
+    node["tourism"="spa_resort"];
+    way["tourism"="spa_resort"];
+    relation["tourism"="spa_resort"];
+
+    node["leisure"="hot_spring"];
+    way["leisure"="hot_spring"];
+    relation["leisure"="hot_spring"];
+
+    node["leisure"="sauna"];
+    way["leisure"="sauna"];
+    relation["leisure"="sauna"];
+
+    node["leisure"="spa"];
+    way["leisure"="spa"];
+    relation["leisure"="spa"];
+
+    node["natural"="hot_spring"];
+    way["natural"="hot_spring"];
+    relation["natural"="hot_spring"];
+
+    node["bath:type"="hot_spring"];
+    way["bath:type"="hot_spring"];
+    relation["bath:type"="hot_spring"];
+
+    node["bath:type"="onsen"];
+    way["bath:type"="onsen"];
+    relation["bath:type"="onsen"];
+
+    node["bath:type"="thermal"];
+    way["bath:type"="thermal"];
+    relation["bath:type"="thermal"];
+
+);
+out body;
+' http://overpass-api.de/api/interpreter -o output_bath_type_thermal.json
